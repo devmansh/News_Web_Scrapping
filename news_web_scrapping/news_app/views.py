6b75825ce50338e100bddc3c7aac872430dc8083
manojsharma1 <93111552+manojsharma1@users.noreply.github.com>
2022-03-29 22:17:12 +0530

    Second Commit
    Home.html, urls and views updated

--- news_web_scrapping/news_app/views.py
+++ news_web_scrapping/news_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import requests
 
+
+API_KEY = 'f1e9a926ed2744c6892dc6bd49b69a89'
 # Create your views here.
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
+    context = {
+        'articles' : articles   
+    }
+    
+    return render(request, 'news_api/home.html', context)
